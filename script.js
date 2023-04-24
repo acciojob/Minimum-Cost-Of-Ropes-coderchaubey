4451da340f4a6006f22d327a1e3ded1213cf75ba
coderchaubey <115737868+coderchaubey@users.noreply.github.com>
2023-04-25 02:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+  var btn=document.getElementById("rope-lengths").value;
+	var btnArr=btn.split(',').map(parseint);
+
+
+	btnArr.sort(function(a,b) {
+		return a-b;
+	});
+
+	var totalCost=0;
+	while (btnArr.length>1) {
+
+		let a=btnArr[0]+btnArr[1];
+		
+		btnArr=btnArr.slice(2);
+
+		totalCost+=a;
+
+		btnArr.push(a);
+		
+		//again sorting
+		btnArr.sort(function(a,b) {
+		return a-b;
+	});
+
+		
+	}
+
+	document.getElementById("result").innerHTML=totalCost;
 }  
+function parseint(elements) {
+	return parseInt(elements);
+}
