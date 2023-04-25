dc2936c0a6a07ae54f83626be5be40e3686d0708
coderchaubey <115737868+coderchaubey@users.noreply.github.com>
2023-04-25 21:56:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 function calculateMinCost() {
-  var btn = document.getElementById("rope-lengths").value;
-  var btnArr = btn.split(',').map(function(params) {
-    return parseInt(params);
-  });
-
-  btnArr.sort(function(a, b) {
-    return a - b;
-  });
-
-  var totalCost = 0;
-  while (btnArr.length > 1) {
-    let a = btnArr[0] + btnArr[1];
-
-    btnArr = btnArr.slice(2);
-
-    totalCost += a;
-
-    btnArr.push(a);
+  try {
+    var btn = document.getElementById("rope-lengths").value;
+    var btnArr = btn.split(',').map(function(params) {
+      return parseInt(params);
+    });
 
     btnArr.sort(function(a, b) {
       return a - b;
     });
-  }
-	totalCost=totalCost.toString();
 
-  document.getElementById("result").innerHTML = totalCost;
+    var totalCost = 0;
+    while (btnArr.length > 1) {
+      let a = btnArr[0] + btnArr[1];
+
+      btnArr = btnArr.slice(2);
+
+      totalCost += a;
+
+      btnArr.push(a);
+
+      btnArr.sort(function(a, b) {
+        return a - b;
+      });
+    }
+
+    totalCost = totalCost.toString();
+
+    document.getElementById("result").innerHTML = totalCost;
+  } catch (err) {
+    // Handle the error, e.g. log it, display an error message, etc.
+    console.error(err);
+    // Cypress will automatically fail the test
+    throw err;
+  }
 }
